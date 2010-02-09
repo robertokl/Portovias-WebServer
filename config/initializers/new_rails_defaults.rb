2fb040025bc2e2ec63bd13a5052ad4cac18312b7
Roberto Klein <klein@klein-laptop.(none)>
2010-02-08 23:16:58 -0200

    first

--- config/initializers/new_rails_defaults.rb
+++ config/initializers/new_rails_defaults.rb
@@ -3,7 +3,13 @@
 # These settings change the behavior of Rails 2 apps and will be defaults
 # for Rails 3. You can remove this initializer when Rails 3 is released.
 
+if defined?(ActiveRecord)
+  # Include Active Record class name as root for JSON serialized output.
+  ActiveRecord::Base.include_root_in_json = true
 
+  # Store the full class name (including module namespace) in STI type column.
+  ActiveRecord::Base.store_full_sti_class = true
+end
 
 ActionController::Routing.generate_best_match = false
 
