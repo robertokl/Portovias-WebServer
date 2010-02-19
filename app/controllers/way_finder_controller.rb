6943111d00a1d73216c2d3e326f92435be838fbf
Roberto Klein <klein@dev_infra01.fabrica.locaweb.com.br>
2010-02-19 17:26:07 -0200

    adixionando formato txt

--- app/controllers/way_finder_controller.rb
+++ app/controllers/way_finder_controller.rb
@@ -1,12 +1,22 @@
 class WayFinderController < ApplicationController
 	require "#{RAILS_ROOT}/lib/portovias.rb"
 	def find_way
-		html = ""
-		ways = find_best_way
-		ways.keys.each do |key|
-			html << "<h4>#{key}</h4>"
-			html << "#{ways[key][1]}: #{ways[key][0]}<p/>"
-		end
-		render :text => html
+	  ways = find_best_way
+    response = ""
+	  respond_to do |format|
+  	  format.html do
+       	ways.keys.each do |key|
+       		response << "<h4>#{key}</h4>"
+       		response << "#{ways[key][1]}: #{ways[key][0]}<p/>"
+       	end
+  	  end
+  	  format.xml do
+      	ways.keys.each do |key|
+      		response << "#{key}\n"
+      		response << "#{ways[key][1]}: #{ways[key][0]}\n\n"
+      	end  	   
+  	  end  
+  	  render :text => response
+	  end
 	end
 end
